7ea1e4876632884d60ba2a38897c78c660e88be9
AlecH7 <henr9847@stthomas.edu>
2019-05-09 22:22:28 +0000

    function updated

--- R/fbind.R
+++ R/fbind.R
@@ -1,5 +1,10 @@
 Director.Gross <- Movie_Data %>% 
-  filter(Movie_Data$content_rating == "PG-13")
+  group_by(director_name) %>% 
+  summarise(Mean.Gross = mean(gross, na.rm = TRUE), 
+            Mean.Budget = mean(budget, na.rm = TRUE)) %>%
+  mutate(Net = Mean.Gross - Mean.Budget) %>% 
+  arrange(desc(Net))
+
 
 ggplot(PG13movies, aes(x = duration, y = gross, colour = imdb_score)) + 
   geom_point()
