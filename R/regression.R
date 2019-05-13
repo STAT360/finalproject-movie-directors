883d2c3d29e0770806eafbe62188be2d3dffa59d
AlecH7 <henr9847@stthomas.edu>
2019-05-13 18:46:36 +0000

    updated regression with more info

--- R/regression.R
+++ R/regression.R
@@ -8,7 +8,22 @@ ReducedMovies <- Movie_Data %>%
 
 
 multiple.mod = lm(gross ~ director_name + budget + duration + actor_1_name + content_rating + imdb_score + title_year, data = ReducedMovies)
-coef(multiple.mod) 
+(coef(multiple.mod))
 anova(multiple.mod)
 
 coef(multiple.mod)["Movie_Data$title_year"]
+coef(multiple.mod)[661:664]
+
+gross.mod = lm(gross ~ director_name, data = ReducedMovies)
+(coef(gross.mod))
+anova(gross.mod)
+max(coef(gross.mod))
+
+
+imdb.mod = lm(imdb_score ~ director_name, data = ReducedMovies)
+(coef(imdb.mod))
+anova(imdb.mod)
+max(coef(imdb.mod))
+
+
+
