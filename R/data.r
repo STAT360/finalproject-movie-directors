fcd49d60b4820f5277e02170abf3cb8029983e06
“hart4847” <hart4847@stthomas.edu>
2019-05-16 00:40:49 +0000

    Started to format information for ?Movie_Data command to view Help pane with a description of our data sets and descriptions of its variables.

---
+++ R/data.r
@@ -0,0 +1,37 @@
+#' IMDB Dataset
+#' 
+#' Dataset from IMDB containing data on movies including directors, actors, year, gross, etc.
+#' 
+#' @format A data frame with 5043 rows and 28 variables:
+#' /describe{
+#'  /item {color}{}
+#'  /item {director_name}{}
+#'  /item {num_critic_for_reviews}{}
+#'  /item {duration}{}
+#'  /item {director_facebook_likes}{}
+#'  /item {actor_3_facebook_likes}{}
+#'  /item {actor_2_name}{}
+#'  /item {actor_1_facebook_likes}{}
+#'  /item {gross}{}
+#'  /item {genres}{}
+#'  /item {actor_1_name}{}
+#'  /item {movie_title}{}
+#'  /item {num_voted_users}{}
+#'  /item {cast_total_facebook_likes}{}
+#'  /item {actor_3_name}{}
+#'  /item {facenumber_in_poster}{}
+#'  /item {plot_keywords}{}
+#'  /item {movie_imdb_link}{}
+#'  /item {num_user_for_reviews}{}
+#'  /item {language}{}
+#'  /item {country}{}
+#'  /item {content_rating}{}
+#'  /item {budget}{}
+#'  /item {title_year}{}
+#'  /item {actor_2_facebook_likes}{}
+#'  /item {imdb_score}{}
+#'  /item {aspect_ratio}{}
+#'  /item {movie_facebook_likes}{}
+#' @references 
+#' }
+"Movie_Data"
