b4d4218f9862e3e8911aa2c77b6007f438ae2d2d
“hart4847” <hart4847@stthomas.edu>
2019-05-16 01:18:19 +0000

    Descriptions added.

--- R/data.r
+++ R/data.r
@@ -4,34 +4,34 @@
 #' 
 #' @format A data frame with 5043 rows and 28 variables:
 #' /describe{
-#'  /item {color}{}
-#'  /item {director_name}{}
+#'  /item {color}{Black and white or colored display}
+#'  /item {director_name}{First and last name of director}
 #'  /item {num_critic_for_reviews}{}
-#'  /item {duration}{}
-#'  /item {director_facebook_likes}{}
+#'  /item {duration}{Duration of movie in minutes}
+#'  /item {director_facebook_likes}{Number of facebook likes on the director's FB page}
 #'  /item {actor_3_facebook_likes}{}
 #'  /item {actor_2_name}{}
-#'  /item {actor_1_facebook_likes}{}
-#'  /item {gross}{}
-#'  /item {genres}{}
-#'  /item {actor_1_name}{}
+#'  /item {actor_1_facebook_likes}{Number of facebook likes on the Actor_1's FB page}
+#'  /item {gross}{Earnings of movies (mostly in US dollars)}
+#'  /item {genres}{Category of movie}
+#'  /item {actor_1_name}{First and last name of main actor/actress}
 #'  /item {movie_title}{}
 #'  /item {num_voted_users}{}
-#'  /item {cast_total_facebook_likes}{}
+#'  /item {cast_total_facebook_likes}{Total FB likes on cast's FB pages}
 #'  /item {actor_3_name}{}
 #'  /item {facenumber_in_poster}{}
-#'  /item {plot_keywords}{}
-#'  /item {movie_imdb_link}{}
+#'  /item {plot_keywords}{Identifying keywords used in movie}
+#'  /item {movie_imdb_link}{URL to IMDB page of movie}
 #'  /item {num_user_for_reviews}{}
 #'  /item {language}{}
 #'  /item {country}{}
-#'  /item {content_rating}{}
-#'  /item {budget}{}
-#'  /item {title_year}{}
+#'  /item {content_rating}{Parental rating of a given movie}
+#'  /item {budget}{Budget of movie (mostly in US dollars)}
+#'  /item {title_year}{Movie release year}
 #'  /item {actor_2_facebook_likes}{}
-#'  /item {imdb_score}{}
+#'  /item {imdb_score}{IMDB score on a scale of 1-10}
 #'  /item {aspect_ratio}{}
-#'  /item {movie_facebook_likes}{}
+#'  /item {movie_facebook_likes}{Number of likes on movie's FB page}
 #' @references https://data.world/data-society/imdb-5000-movie-dataset
 #' }
 "Movie_Data"
